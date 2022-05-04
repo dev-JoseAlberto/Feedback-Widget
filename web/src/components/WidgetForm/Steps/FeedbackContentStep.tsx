0c9490b92acaaa16a40975e0ecc21e022e47dd79
dev-JoseAlberto <jsalberto2016@outlook.com>
2022-05-04 12:53:29 -0300

    I'm creating interfaces.

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -2,6 +2,7 @@ import { ArrowLeft, Camera } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from "../../WidgetForm/index";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
+import { useState } from "react";
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
@@ -12,6 +13,8 @@ export function FeedbackContentStep ({
     feedbackType, 
     onFeedbackRestartRequested 
 }: FeedbackContentStepProps) {
+    const [screenshot, setScreenshot] = useState<string | null>(null)
+    
     const feedbackTypeInfo = feedbackTypes[feedbackType];
 
     return (
@@ -37,7 +40,7 @@ export function FeedbackContentStep ({
             />
 
             <footer className="flex gap-2 mt-2">
-                <ScreenshotButton />
+                <ScreenshotButton onScreenshotTook={setScreenshot}/>
                 <button 
                 type="submit"
                 className="p-2 bg-[#0d0c22] rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-blue-700 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-offset-[#091F8E] focus:ring-[#091F8E] transition-colors">
