6a9ffc7e2f99bbce15a816baa9b209fa526b38d9
dev-JoseAlberto <jsalberto2016@outlook.com>
2022-05-05 12:42:00 -0300

    I'm setting the application architecture for React.

---
+++ web/server/src/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { prisma } from "./prisma";
+
+const app = express();
+
+app.use(express.json());
+
+
+app.post('/feedbacks', async (request, response) => {
+    const { type, comment, screenshot } = request.body;
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot,
+        }
+    })
+
+    return response.status(201).json({data:feedback});
+
+});
+
+app.listen(3333, () => {
+    console.log('HTTP server running!');
+});
