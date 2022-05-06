616d78f70eeb0ad61d198e6333a66725d79f3fa3
dev-JoseAlberto <jsalberto2016@outlook.com>
2022-05-06 14:28:08 -0300

    I'm creating the dependency inversion using Typescript.

--- web/server/src/routers.ts
+++ web/server/src/routers.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import nodemailer from "nodemailer";
 import { prisma } from "./prisma";
+import { PrismaFeedbackRepository } from "./repositories/prisma/prisma-feedbacks-repository";
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 
 export const routers = express.Router()
 
@@ -16,28 +18,31 @@ const transport = nodemailer.createTransport({
 
 
 routers.post('/feedbacks', async (request, response) => {
-    //const { type, comment, screenshot } = request.body;
+    const { type, comment, screenshot } = request.body;
+    
+    const prismaFeedbackRepository = new PrismaFeedbackRepository()
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+        prismaFeedbackRepository
+    )
 
-    const feedback = await prisma.feedback.create({
-        data: {
-            type:request.body.type,
-            comment:request.body.comment,
-            screenshot: request.body.screenshot,
-        }
+    await submitFeedbackUseCase.execute({
+        type,
+        comment,
+        screenshot,
     })
     
-  await transport.sendMail({
-    from:'Team feedback <hello@feedget.com>',
-    to: 'Jose Alberto <alberto.05.lq1@gmail.com>',
-    subject: 'New feedback',
-    html: [
-      '<div style="font-family: sans-serif; font-size: 16px; color:#111;">',
-      '<p>Type feedback: ${type}</p>',
-      '<p>Comment: ${comment}</p>',
-      '</div>'
-    ].join('\n')
-  });
+  //await transport.sendMail({
+    //from:'Team feedback <hello@feedget.com>',
+    //to: 'Jose Alberto <alberto.05.lq1@gmail.com>',
+    //subject: 'New feedback',
+    //html: [
+      //'<div style="font-family: sans-serif; font-size: 16px; color:#111;">',
+      //'<p>Type feedback: ${type}</p>',
+      //'<p>Comment: ${comment}</p>',
+      //'</div>'
+    //].join('\n')
+ // });
 
-  return response.status(201).json({data:feedback});
+  return response.status(201).send();
 
 });
