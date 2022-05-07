aa5aaa47deb1cf0e031c6fc3dbc3946c2e2b227f
dev-JoseAlberto <jsalberto2016@outlook.com>
2022-05-07 11:53:30 -0300

    I'm setting the cors.

--- web/server/src/server.ts
+++ web/server/src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import { routers } from "./routers";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routers);
 
